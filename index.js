37207cdc478c7ead2e6fa46ae643a764b1708468
ProGamerLV69 <107753790+ProGamerLV69@users.noreply.github.com>
2022-06-18 19:06:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.loadPlugin(cmd)
 
 bot.on('login',function(){
 	console.log("Logged In")
-	bot.chat("hello");
+	bot.chat("https://youtu.be/xm3YgoEiEDc");
 });
 
 bot.on('time', function(time) {
